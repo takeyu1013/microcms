026157dc6f98aeaa7009c8d7363153bb09d253ce
takeyu1013 <yuto1013@gmail.com>
2022-08-23 20:52:58 +0900

    🎉

---
+++ src/libs/client.ts
@@ -0,0 +1,7 @@
+import { createClient } from "microcms-js-sdk"; //ES6
+
+// Initialize Client SDK.
+export const client = createClient({
+  serviceDomain: "microcms20220823", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
+  apiKey: process.env.API_KEY,
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
+import { client } from "src/libs/client";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+export type Blog = {
+  title: string;
+  body: string;
+};
+
+type Props = MicroCMSListResponse<Blog>;
+
+const Home: NextPage<Props> = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>{`記事の総数: ${props.totalCount}件`}</p>
+      <ul>
+        {props.contents.map((content) => {
+          return (
+            <li key={content.id}>
+              <Link href={`blog/${content.id}`}>
+                <a>{content.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<Blog>({ endpoint: "blog" });
+  return {
+    props: data,
+  };
 };
 
 export default Home;
